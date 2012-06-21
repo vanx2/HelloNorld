6ecbbbfe60891aec27d94d76f19d7b3e0ab1c6bb
vanx2 <vanxx2@gmail.com>
2012-06-21 18:52:10 +0900

    gogo

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!!!!!!!!!!!\n');
-}).listen(80);
+}).listen(8080);
+
+var a="";
 
